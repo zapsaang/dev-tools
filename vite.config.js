80de94a92f7cc608d7dc681a28aa9400d2a0501e
ZapSaang <admin@zapsaang.com>
2025-03-25 19:45:37 +0800

    build: update vite config with rollup and optimizeDeps options
    Add rollupOptions to specify the input file and include 'vue-i18n' in optimizeDeps to improve build performance and dependency optimization.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/'
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['vue-i18n']
+  }
 })
