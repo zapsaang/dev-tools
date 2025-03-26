01050f925e9a70511c357b540dd4cac24eb67ea5
ZapSaang <admin@zapsaang.com>
2025-03-26 15:09:47 +0800

    chore: remove i18n and devtools dependencies
    Remove unused i18n and devtools dependencies to simplify the project and reduce bundle size. This includes removing related imports and configurations in the codebase.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import { createApp } from 'vue'
 import './styles/common.css'
 import App from './App.vue'
-import { i18n } from './i18n'
+
 
 // Remove i18n import
 // Remove createI18n initialization
 // Remove app.use(i18n)
 const app = createApp(App)
-app.use(i18n)
 app.mount('#app')
